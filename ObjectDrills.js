df51f7b2d59f9b0b8fc2a56b4075d464854bbeab
Orkun Ozekin <orkunozekin@thinkful.com>
2020-03-17 16:23:05 -0500

    obj-keys

--- ObjectDrills.js
+++ ObjectDrills.js
@@ -1,14 +1,27 @@
-'use strict';
+'use strict'; 
 
-const loaf = {
-  flour: 300,
-  water: 210
+// const loaf = {
+//   flour: 300,
+//   water: 210
+// };
+
+// console.log(loaf);
+
+// loaf.hydration = function() {
+//   return (this.water / this.flour) * 100;
+// };
+
+// console.log(loaf.hydration());
+
+
+let obj = {
+  foo : 'red',
+  bar : 'bar',
+  fum : 'fum',
+  quux : 'quux',
+  spam : 'spam'
 };
 
-console.log(loaf);
-
-loaf.hydration = function() {
-  return (this.water / this.flour) * 100;
-};
-
-console.log(loaf.hydration());
+for(let key in obj) {
+  console.log(key + ':' + obj[key])
+}
